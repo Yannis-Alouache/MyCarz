f316859d2442d67a3070a7ac7f353e5c1e6efc09
Yannis-Alouache <alouacehyannis1@gmail.com>
2023-12-02 21:36:03 +0100

    [~] fixed whatsapp badge

--- frontend/src/app/_components/WhatsappBadge/WhatsappBadge.jsx
+++ frontend/src/app/_components/WhatsappBadge/WhatsappBadge.jsx
@@ -8,7 +8,7 @@ const WhatsappBadge = () => {
         data-action="open" data-phone="33620081250"
         data-message="Bonjour&nbsp;! J'ai une question concernant vos prestations avec MyCarz :"
         role="button" tabindex="0" target="_blank" href='tel:33620081250'
-        className='flex items-center gap-x-2 bg-[#25d366] text-white py-2 px-5 font-bold rounded-full fixed right-3 bottom-3 z-10'>
+        className='flex items-center gap-x-2 bg-[#25d366] text-white py-2 px-5 font-bold rounded-full fixed right-3 bottom-3 z-50'>
             <FaWhatsapp size={25}/>
             <span>Besoin d'informations ?</span>
     </a>
