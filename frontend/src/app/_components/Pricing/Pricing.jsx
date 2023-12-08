679a20d985394b38e150ce09dbfe8cade041a49b
Yannis-Alouache <alouacehyannis1@gmail.com>
2023-12-08 16:01:12 +0100

    [~] rework princing system

--- frontend/src/app/_components/Pricing/Pricing.jsx
+++ frontend/src/app/_components/Pricing/Pricing.jsx
@@ -1,6 +1,11 @@
+import { AiOutlineClose } from "react-icons/ai";
+import { FaCheck } from "react-icons/fa";
 
 const Pricing = (props) => {
-    let { title, price, services, catchPhrase, mainColor, noEuroSign } = props
+  let { title, price, services, catchPhrase, mainColor, noEuroSign } = props
+
+  
+
   return (
     <>
         <div className={"flex flex-col p-6 mx-auto max-w-lg text-center text-gray-900 bg-white rounded-lg w-full border shadow-xl xl:p-8"} style={{borderColor: mainColor}}>
@@ -9,12 +14,18 @@ const Pricing = (props) => {
             <div className="flex justify-center items-baseline my-8">
                 <span className="mr-2 text-5xl font-extrabold">{price}{noEuroSign ? "" : "€"}</span>
             </div>
-            <ul role="list" className="mb-8 space-y-4 text-left">
+            <ul role="list" className="mb-8 space-y-2 text-left">
                 {services.map(service => {
+                    let svg = null;
+                    if (service.included)
+                        svg = <FaCheck className="text-green-500" />
+                    else
+                        svg = <AiOutlineClose className="text-red-500" />
                     return (
                         <li className="flex items-center space-x-3" key={service}>
-                            <svg className="flex-shrink-0 w-5 h-5 text-green-500" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z" clipRule="evenodd"></path></svg>
-                            <span>{service}</span>
+                            {svg}
+                            {/* <svg className="flex-shrink-0 w-5 h-5 text-green-500" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z" clipRule="evenodd"></path></svg> */}
+                            <span>{service.text}</span>
                         </li>
                     )
                 })}
