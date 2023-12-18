7b4c4e66b63d96fd3dd55870ea6bed5f72c12665
Yannis-Alouache <alouacehyannis1@gmail.com>
2023-12-18 09:11:54 +0100

    [~] tried fix service responsive

--- frontend/src/app/_components/Service/Service.jsx
+++ frontend/src/app/_components/Service/Service.jsx
@@ -3,39 +3,24 @@
 import Image from "next/image";
 import Arrow from "@/app/_assets/Arrow.webp"
 import Button from "../Button/Button";
-
+import { useState, useEffect } from "react";
 const Service = (props) => {
 
-  let { title, link, mainColor, secondaryColor, text, mainImage, svg, svgClassName, showBtn, reverse } = props;
-  console.log(window.innerWidth)
+  let { title, link, mainColor, secondaryColor, text, mainImage, showBtn, reverse } = props;
+  const size = useWindowSize();
 
-  if (typeof window !== "undefined") {
-    let firstCol = `
-    <div className={"lg:col-span-6 col-span-12 " + (reverse === true && window.innerWidth > 640 ? 'order-last' : '')}>
-      <div className="relative">
-        <div className={ "absolute w-[240px] h-[240px] lg:block hidden z-0 " + (reverse === true ? "-right-8 -top-8" : "-left-8 -bottom-8" )} style={{ backgroundImage: "linear-gradient(" + mainColor + "," + secondaryColor + ")" }}></div>
-        <Image className={"relative z-10 " + (reverse === true && window.innerWidth > 640 ? 'float-right mt-5' : '')} alt="" src={mainImage} />
-      </div>
-    </div>
-    `
-  }
-
+  console.log(size)
   return (
     <div className="relative lg:py-28 py-16">
       {/* <Image src={ svg } alt="" className={"absolute z-0 w-auto h-full " + svgClassName } /> */}
       <div className="max-w-screen-xl px-10 w-full mx-auto z-10 relative">
         <div className="grid grid-cols-12 lg:gap-x-10 gap-x-0">
-        {typeof window !== "undefined" ?
-          <div className={"lg:col-span-6 col-span-12 " + (reverse === true && window.innerWidth > 640 ? 'order-last' : '')}>
+          <div className={"lg:col-span-6 col-span-12 " + (reverse === true && size.width > 640 ? 'order-last' : '')}>
             <div className="relative">
               <div className={ "absolute w-[240px] h-[240px] lg:block hidden z-0 " + (reverse === true ? "-right-8 -top-8" : "-left-8 -bottom-8" )} style={{ backgroundImage: "linear-gradient(" + mainColor + "," + secondaryColor + ")" }}></div>
-              <Image className={"relative z-10 " + (reverse === true && window.innerWidth > 640 ? 'float-right mt-5' : '')} alt="" src={mainImage} />
+              <Image className={"relative z-10 " + (reverse === true && size.width > 640 ? 'float-right mt-5' : '')} alt="" src={mainImage} />
             </div>
           </div>
-          :
-          <div></div>
-        }
-          
           <div className="lg:col-span-6 col-span-12 flex flex-col justify-center">
             <h2 className="text-4xl uppercase font-black mb-3 mt-5">{ title }</h2>
             <div className="flex gap-x-10 mb-3">
@@ -46,11 +31,41 @@ const Service = (props) => {
               <Button href={link} className={ 'uppercase float-right font-bold w-fit' } style={{ borderColor: mainColor }}>En savoir plus</Button>
             </div>
           </div>
-
         </div>
       </div>
     </div>
   )
+}
+
+function useWindowSize() {
+  // Initialize state with undefined width/height so server and client renders match
+  // Learn more here: https://joshwcomeau.com/react/the-perils-of-rehydration/
+  const [windowSize, setWindowSize] = useState({
+    width: undefined,
+    height: undefined,
+  });
+
+  useEffect(() => {
+    // only execute all the code below in client side
+    // Handler to call on window resize
+    function handleResize() {
+      // Set window width/height to state
+      setWindowSize({
+        width: window.innerWidth,
+        height: window.innerHeight,
+      });
+    }
+    
+    // Add event listener
+    window.addEventListener("resize", handleResize);
+     
+    // Call handler right away so state gets updated with initial window size
+    handleResize();
+    
+    // Remove event listener on cleanup
+    return () => window.removeEventListener("resize", handleResize);
+  }, []); // Empty array ensures that effect is only run on mount
+  return windowSize;
 }
 
 export default Service
