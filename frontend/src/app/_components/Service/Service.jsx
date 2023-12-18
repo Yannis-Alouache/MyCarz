d75088674b5ee26027433da08c70aa5755ec932f
Yannis-Alouache <alouacehyannis1@gmail.com>
2023-12-18 08:37:43 +0100

    [~] tried to fix

--- frontend/src/app/_components/Service/Service.jsx
+++ frontend/src/app/_components/Service/Service.jsx
@@ -8,17 +8,33 @@ const Service = (props) => {
 
   let { title, link, mainColor, secondaryColor, text, mainImage, svg, svgClassName, showBtn, reverse } = props;
   console.log(window.innerWidth)
+
+  if (typeof window !== "undefined") {
+    let firstCol = `
+    <div className={"lg:col-span-6 col-span-12 " + (reverse === true && window.innerWidth > 640 ? 'order-last' : '')}>
+      <div className="relative">
+        <div className={ "absolute w-[240px] h-[240px] lg:block hidden z-0 " + (reverse === true ? "-right-8 -top-8" : "-left-8 -bottom-8" )} style={{ backgroundImage: "linear-gradient(" + mainColor + "," + secondaryColor + ")" }}></div>
+        <Image className={"relative z-10 " + (reverse === true && window.innerWidth > 640 ? 'float-right mt-5' : '')} alt="" src={mainImage} />
+      </div>
+    </div>
+    `
+  }
+
   return (
     <div className="relative lg:py-28 py-16">
       {/* <Image src={ svg } alt="" className={"absolute z-0 w-auto h-full " + svgClassName } /> */}
       <div className="max-w-screen-xl px-10 w-full mx-auto z-10 relative">
         <div className="grid grid-cols-12 lg:gap-x-10 gap-x-0">
+        {typeof window !== "undefined" ?
           <div className={"lg:col-span-6 col-span-12 " + (reverse === true && window.innerWidth > 640 ? 'order-last' : '')}>
             <div className="relative">
               <div className={ "absolute w-[240px] h-[240px] lg:block hidden z-0 " + (reverse === true ? "-right-8 -top-8" : "-left-8 -bottom-8" )} style={{ backgroundImage: "linear-gradient(" + mainColor + "," + secondaryColor + ")" }}></div>
               <Image className={"relative z-10 " + (reverse === true && window.innerWidth > 640 ? 'float-right mt-5' : '')} alt="" src={mainImage} />
             </div>
           </div>
+          :
+          <div></div>
+        }
           
           <div className="lg:col-span-6 col-span-12 flex flex-col justify-center">
             <h2 className="text-4xl uppercase font-black mb-3 mt-5">{ title }</h2>
