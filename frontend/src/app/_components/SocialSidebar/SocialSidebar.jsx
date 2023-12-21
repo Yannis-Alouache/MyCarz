4d12cf95aef6f93b93e9faaea357532271fd3c50
Yannis-Alouache <alouacehyannis1@gmail.com>
2023-12-21 10:17:42 +0100

    [~]

--- frontend/src/app/_components/SocialSidebar/SocialSidebar.jsx
+++ frontend/src/app/_components/SocialSidebar/SocialSidebar.jsx
@@ -3,6 +3,7 @@ import Instagram from "@/app/_assets/instagram-logo.png"
 import Linkedin from "@/app/_assets/linkedin-logo.png"
 import Facebook from "@/app/_assets/facebook-logo.png"
 import Youtube from "@/app/_assets/youtube-logo.png"
+import Whatsapp from "@/app/_assets/whatsapp-logo.png"
 import Snapchat from "@/app/_assets/snapchat-logo.png"
 import Image from "next/image";
 
@@ -33,30 +34,33 @@ const SocialSidebar = () => {
         </div>
 
 
-        <div className="block lg:hidden fixed bottom-0 z-50">
-            <ul className="flex">
-                <li>
-                    <a href="https://www.facebook.com/mycarzfr" className="transition-all ease-in-out duration-300 hover:opacity-50" target="_blank"><Image src={Facebook} className="w-[25px] h-[25px]"/></a>
-                </li>
-                <li>
-                    <a href="https://www.instagram.com/mycarzfr/" className="transition-all ease-in-out duration-300 hover:opacity-50" target="_blank"><Image src={Instagram} className="w-[25px] h-[25px]" /></a>
-                </li>
-                <li>
-                    <a href="https://www.youtube.com/channel/UCHBrNQofk_t62s3-2hQe9fQ" className="transition-all ease-in-out duration-300 hover:opacity-50" target="_blank"><Image src={Youtube} className="w-[25px] h-full" /></a>
-                </li>
-                <li>
-                    <a href="https://www.linkedin.com/company/mycarzfr/" className="transition-all ease-in-out duration-300 hover:opacity-50" target="_blank"><Image src={Linkedin} className="w-[25px] h-[25px]" /></a>
-                </li>
-                <li>
-                    <a href="https://www.tiktok.com/@mycarzfr" className="transition-all ease-in-out duration-300 hover:opacity-50" target="_blank"><Image src={TikTok} className="w-[25px] h-[25px]" /></a>
-                </li>
-                <li>
-                    <a href="https://www.snapchat.com/add/mycarzfr" className=" transition-all ease-in-out duration-300 hover:opacity-50" target="_blank"><Image src={Snapchat} className="w-[25px] h-[25px]" /></a>
-                </li>
-                <li>
-                    <a href="https://api.whatsapp.com/send?phone=+33620081250" className=" transition-all ease-in-out duration-300 hover:opacity-50" target="_blank"><Image src={Snapchat} className="w-[25px] h-[25px]" /></a>
-                </li>
-            </ul>
+        <div className="block lg:hidden mx-auto fixed inset-x-0 bottom-2 z-50 px-5">
+            <div className="bg-[#EEEEEE] border-2 shadow-md border-gray rounded-full py-2 px-4">
+                <ul className="flex justify-between items-center">
+                    <li>
+                        <a href="https://www.facebook.com/mycarzfr" className="transition-all ease-in-out duration-300 hover:opacity-50" target="_blank"><Image src={Facebook} className="w-[25px] h-[25px]"/></a>
+                    </li>
+                    <li>
+                        <a href="https://www.instagram.com/mycarzfr/" className="transition-all ease-in-out duration-300 hover:opacity-50" target="_blank"><Image src={Instagram} className="w-[25px] h-[25px]" /></a>
+                    </li>
+                    <li>
+                        <a href="https://www.youtube.com/channel/UCHBrNQofk_t62s3-2hQe9fQ" className="transition-all ease-in-out duration-300 hover:opacity-50" target="_blank"><Image src={Youtube} className="w-[25px] h-full" /></a>
+                    </li>
+                    <li>
+                        <a href="https://www.linkedin.com/company/mycarzfr/" className="transition-all ease-in-out duration-300 hover:opacity-50" target="_blank"><Image src={Linkedin} className="w-[25px] h-[25px]" /></a>
+                    </li>
+                    <li>
+                        <a href="https://www.tiktok.com/@mycarzfr" className="transition-all ease-in-out duration-300 hover:opacity-50" target="_blank"><Image src={TikTok} className="w-[25px] h-[25px]" /></a>
+                    </li>
+                    <li>
+                        <a href="https://www.snapchat.com/add/mycarzfr" className=" transition-all ease-in-out duration-300 hover:opacity-50" target="_blank"><Image src={Snapchat} className="w-[25px] h-[25px]" /></a>
+                    </li>
+                    <li>
+                        <a href="https://api.whatsapp.com/send?phone=+33620081250" className=" transition-all ease-in-out duration-300 hover:opacity-50" target="_blank"><Image src={Whatsapp} className="w-[25px] h-[25px]" /></a>
+                    </li>
+                </ul>
+
+            </div>
         </div>
     </>
   )
