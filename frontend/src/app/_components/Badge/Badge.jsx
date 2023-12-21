09f3a72b7ebb0445ae91fd6a6437ad5d36a83140
Yannis-Alouache <alouacehyannis1@gmail.com>
2023-12-21 10:30:08 +0100

    [~]

--- frontend/src/app/_components/Badge/Badge.jsx
+++ frontend/src/app/_components/Badge/Badge.jsx
@@ -3,7 +3,7 @@ import { BsTelephoneFill } from 'react-icons/bs';
 const Badge = (props) => {
   let { className } = props
   return (
-    <div className={"rounded-xl px-20 py-1 text-white flex items-center justify-center w-fit lg:border-none border-2 border-white " + className}>
+    <div className={"rounded-xl lg:px-20 px-5 py-1 text-white flex items-center justify-center lg:w-fit w-full lg:border-none border-2 border-white " + className}>
         <div className="flex justify-center items-center gap-x-[10px]">
             <BsTelephoneFill />
             <span className="font-extrabold">03 76 11 59 59</span>
