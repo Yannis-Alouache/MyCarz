a0436c4419c1309401d246110354f596b52b2c0e
Yannis-Alouache <alouacehyannis1@gmail.com>
2024-01-10 08:41:53 +0100

    [+] added redirect on click

--- frontend/src/app/_components/Pricing/Pricing.jsx
+++ frontend/src/app/_components/Pricing/Pricing.jsx
@@ -22,7 +22,7 @@ let { title, price, services, catchPhrase, mainColor, noEuroSign } = props
                     )
                 })}
             </ul>
-            <a href="#" className={"text-white hover:bg-primary-700 focus:ring-4 focus:ring-primary-200 font-medium rounded-lg text-sm px-5 py-2.5 text-center "} style={{background: mainColor}}>Nous Contacter</a>
+            <a href="/contact" className={"text-white hover:bg-primary-700 focus:ring-4 focus:ring-primary-200 font-medium rounded-lg text-sm px-5 py-2.5 text-center "} style={{background: mainColor}}>Nous Contacter</a>
         </div>
     </Reveal>
   )
--- frontend/src/app/_components/PricingWithSvg/PricingWithSvg.jsx
+++ frontend/src/app/_components/PricingWithSvg/PricingWithSvg.jsx
@@ -32,7 +32,7 @@ const PricingWithSvg = (props) => {
                         )
                     })}
                 </ul>
-                <a href="#" className={"text-white hover:bg-primary-700 focus:ring-4 focus:ring-primary-200 font-medium rounded-lg text-sm px-5 py-2.5 text-center "} style={{background: mainColor}}>Nous Contacter</a>
+                <a href="/contact" className={"text-white hover:bg-primary-700 focus:ring-4 focus:ring-primary-200 font-medium rounded-lg text-sm px-5 py-2.5 text-center "} style={{background: mainColor}}>Nous Contacter</a>
             </div>
         </Reveal>
     </>
