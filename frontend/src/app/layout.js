679e0b2e3ae136b7c966a1978c3ab9a39627689f
Yannis Alouache <57330326+Yannis-Alouache@users.noreply.github.com>
2024-06-14 09:03:25 +0200

    Update layout.js

--- frontend/src/app/layout.js
+++ frontend/src/app/layout.js
@@ -6,38 +6,6 @@ import { Inter } from 'next/font/google'
 
 const inter = Inter({ subsets: ['latin'] })
 
-export const metadata = {
-  metadataBase: new URL('https://mycarz.fr/'),
-  title: 'MyCarz',
-  applicationName: 'My Carz',
-  description: 'My Carz : Garage mécanique / Lavage auto / Carte grise / Achat-Revente / Covering et bien plus encore chez MyCarz !',
-  creator: 'Yannis Alouache',
-  publisher: 'Yannis Alouache',
-  authors: [{name: "Yannis Alouache", url: "https://yannis-alouache.netlify.app/"}],
-  keywords: ["Garage Roubaix", "Lavage Auto Roubaix"],
-  alternates: {
-    canonical: '/',
-  },
-  openGraph: {
-    title: 'MyCarz',
-    description: 'My Carz : Garage mécanique / Lavage auto / Carte grise / Achat-Revente / Covering et bien plus encore chez MyCarz !',
-    url: "/",
-    siteName: "My Carz",
-    locale: "fr_FR",
-    type: "website",
-  },
-  robots: {
-    index: true,
-    follow: true,
-    nocache: false,
-    googleBot: {
-      index: true,
-      follow: true,
-      noimageindex: true
-    },
-  },
-}
-
 export default function RootLayout({ children }) {
   return (
     <html dir="ltr" lang="fr-FR" style={{scrollBehavior:'smooth'}}>
